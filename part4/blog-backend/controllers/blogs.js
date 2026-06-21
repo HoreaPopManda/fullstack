01dd69c232041b9bd338bc1356cff2ff93855b5a
horea <horeapopmanda@gmail.com>
2026-06-21 22:09:54 +1000

    finished exercises 4.8.-4.12

--- part4/blog-backend/controllers/blogs.js
+++ part4/blog-backend/controllers/blogs.js
@@ -1,31 +1,36 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
-})
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs)
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
+
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+
+})
+
+
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
+
+  if (!body.title || !body.url) {
+    return response.status(400).json({ error: 'title and url are required' })
+  }
 
   const blog = new Blog({
     title: body.title,
@@ -34,40 +39,37 @@ blogsRouter.post('/', (request, response, next) => {
     likes: body.likes || 0,
   })
 
-  blog.save()
-    .then(savedBlog => {
-      response.json(savedBlog)
-    })
-    .catch(error => next(error))
+  const savedBlog = await blog.save()
+  response.json(savedBlog)
 })
 
-blogsRouter.delete('/:id', (request, response, next) => {
-  Blog.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+
+
+blogsRouter.delete('/:id', async (request, response) => {
+
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
-blogsRouter.put('/:id', (request, response, next) => {
+
+
+blogsRouter.put('/:id', async (request, response) => {
   const { title, author, url, likes } = request.body
 
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (!blog) {
-        return response.status(404).end()
-      }
+  const blogToUpdate = await Blog.findById(request.params.id)
 
-      blog.title = title
-      blog.author = author
-      blog.url = url
-      blog.likes = likes
+  if (!blogToUpdate) {
+    return response.status(404).end()
+  }
 
-      return blog.save().then((updatedBlog) => {
-        response.json(updatedBlog)
-      })
-    })
-    .catch(error => next(error))
+  blogToUpdate.title = title
+  blogToUpdate.author = author
+  blogToUpdate.url = url
+  blogToUpdate.likes = likes
+
+  await blogToUpdate.save()
+
+  response.json(blogToUpdate)
 })
 
 module.exports = blogsRouter
--- part4/blog-backend/tests/blog_api.test.js
+++ part4/blog-backend/tests/blog_api.test.js
@@ -26,6 +26,104 @@ test('blogs are returned as json. expect 8 in the test db', async () => {
 })
 
 
+
+test('get single blog and test it has the id', async () => {
+  await api
+    .get('/api/blogs/000000000000000000000001')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+    .expect(res => { assert.strictEqual(res.body.title, 'FBI')})
+    .expect(res => { assert.ok(res.body.id) })
+})
+
+
+
+test('add and delete right after', async () => {
+  const newBlog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'https://example.com/test',
+    likes: 0
+  }
+
+  const resWithID = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  // trace (resWithID.body)
+
+  const id = resWithID.body.id
+
+  await api
+    .put(`/api/blogs/${id}`)
+    .send({ ...newBlog, title: 'Updated Test Blog' })
+    .expect(200)
+
+  await api
+    .get(`/api/blogs/${id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+    .expect(res => { assert.strictEqual(res.body.title, 'Updated Test Blog')})
+
+  await api
+    .delete(`/api/blogs/${id}`)
+    .expect(204)
+
+})
+
+
+test.only('test likes defaults to 0 ', async () => {
+  const newBlog = {
+    title: 'Test Zero likes',
+    author: 'Test Author',
+    url: 'https://example.com/testZeroLikes',
+  }
+
+  const resWithID = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  // trace (resWithID.body)
+
+  const id = resWithID.body.id
+
+  assert.strictEqual(resWithID.body.likes, 0)
+
+  await api
+    .delete(`/api/blogs/${id}`)
+    .expect(204)
+
+})
+
+
+test('test missing data ', async () => {
+  const newBlogNoTitle = {
+    author: 'Test Author',
+    url: 'https://example.com/testZeroLikes',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlogNoTitle)
+    .expect(400)
+
+
+  const newBlogNoUrl = {
+    title: 'Test Blog',
+    author: 'Test Author',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlogNoUrl)
+    .expect(400)
+})
+
+
 after(async () => {
   await mongoose.connection.close()
 })
